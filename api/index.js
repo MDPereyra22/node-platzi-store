44d630362c6b00178c7ececdfbac1280ffe0d311
MDPereyra22 <pereyra.dmatias@gmail.com>
2024-08-01 15:41:58 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -5,16 +5,16 @@ const cors = require('cors')
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3004;
+const port = process.env.PORT || 3004
 
 app.use(express.json());
 app.use(cors())
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hola mi server en express');
 });
 
-app.get('/nueva-ruta', (req, res) => {
+app.get('/api/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
